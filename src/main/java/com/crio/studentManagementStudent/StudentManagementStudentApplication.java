e749be8fd7ba7fe2730042808c633d80c860e83d
Ankit Sen <ankitsenchowdhury@gmail.com>
2024-05-04 23:37:45 +0530

    first commit

---
+++ src/main/java/com/crio/studentManagementStudent/StudentManagementStudentApplication.java
@@ -0,0 +1,13 @@
+package com.crio.studentManagementStudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementStudentApplication.class, args);
+	}
+
+}
